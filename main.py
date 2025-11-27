a946754beb022dcf07ed6acbf00741d3c01d9262
AGozum-10E <gozum.ac@obmontessori.edu.ph>
2025-11-27 08:43:45 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from pyscript import display
+
+weather_today = {    
+        
+        "location": "Manila", 
+        "temperature_c": 27,   
+        "humidity": 85,    
+        "condition": "Partly Sunny"
+    
+    }
